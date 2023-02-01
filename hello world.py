cc487646f9d46ff42dfd61ac759b645b6fcdb57f
josephsurrey <josephsurrey2018@gmail.com>
2023-02-02 11:26:19 +1300

    test

---
+++ hello world.py
@@ -0,0 +1,8 @@
+print('hello word')
+
+joshIsObese = input('Is Joel Yu morbidly obese? y/n ')
+
+if joshIsObese == 'y':
+    print('Correct, Joel Yu is morbidly obsese')
+else:
+    print("Wrong, Joel Yu is morbidly obese")
